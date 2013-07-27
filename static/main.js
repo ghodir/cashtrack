f27169d42400d314c05d4d80bfa272288b3f938f
ghodir <ghodir@aionlabs.de>
2013-07-27 21:40:22 +0200

    Created a SubCollection class for Backbone to take care of the individual groups without loosing the add,remove and change events for the whole collection

--- static/main.js
+++ static/main.js
@@ -1,43 +1,85 @@
 (function() {
-	var Category = Backbone.Model.extend({
-		defaults: {
-			name: '',
-			type: '',
-			icon: '',
-			value: '',
-			budget: '',
-			color: '',
+	var SubCollection = Backbone.Collection.extend({
+		constructor: function( collection, cb, options ) {
+			this.collection = collection;
+			cb && (this.cb = cb);
+			
+			this.listenTo( this.collection, 'add', this._onAdd);
+			this.listenTo( this.collection, 'change', this._onChange);
+			this.listenTo( this.collection, 'remove', this._onRemove);
+			this.listenTo( this.collection, 'reset', this._onReset);
+	
+			var models = this.collection.filter(cb);
+			Backbone.Collection.call(this, models, options);
+		},
+		_onChange: function(model) {
+			if( !this.cb(model) )
+				this.remove( model );
+		},
+		_onAdd: function(model) {
+			if( this.cb(model) )
+				this.add( model );
+		},
+		_onRemove: function(model) {
+			this.remove(model);
+		},
+		_onReset: function() {
+			this.reset( this.collection.filter(this.cb) );
 		}
 	});
 	
-	var CategoriesCollection = Backbone.Collection.extend({
-		url: '/api/users/1/categories',
-		model: Category,
+	var SubGroupCollection = SubCollection.extend({
+		constructor: function(collection, name, value, options) {
+			SubCollection.call(this, collection, function(model) {
+				return model.get(name) === value;
+			}, options);
+		}
 	});
 	
-	var Section = Backbone.Model.extend();
-	var SectionsCollection = Backbone.Collection.extend();
+	var Node = Backbone.Model.extend({});
+	var Nodes = Backbone.Collection.extend({
+		url: '/api/users/1/nodes',
+		model: Node
+	});
 	
-	var View = Backbone.View.extend({
-		template: _.template( $('#template-view').html() ),
-		initialize: function( options ) {
-			options = options || {};
-			
-			this.collection = options.collection;
-			this.collection.on('reset', this.render, this);
+	var SectionView = Backbone.View.extend({
+		className: 'section',
+		initialize: function(options) {
+			this.name = options.name;
+			this.elements = options.elements;
 		},
-		render: function( ) {
-			var sections = this.collection.groupBy('type');
-			_.each( sections, function( categories, name ) {
-				sections[name] = ( new CategoriesCollection(categories)).toJSON()
-			});
-			this.$el.html( this.template({sections: sections}) );
+		render: function() {
+			this.template || (this.template = _.template( $('#template-section').html() )),
+			
+			console.log( 'render section' );
+			this.$el.html( this.template({name: this.name, elements: this.elements.toJSON() }) );
 			return this.$el;
 		}
 	});
 	
-	var categories = new CategoriesCollection();
-		categories.fetch({
+	var HomeScreen = Backbone.View.extend({
+		initialize: function(options) {
+			this.sections = {};
+			_.each(['income', 'account', 'category', 'goal'], function(name) {
+				var c = new SubGroupCollection(options.collection, 'type', name);
+				this.sections[name] = new SectionView({name: name, elements: c});
+			}, this);
+			
+			this.listenTo( options.collection, 'reset', this.render);
+			this.listenTo( options.collection, 'change', this.render);
+		},
+		render: function() {
+			console.log( 'render home screen' );
+			this.$el.html('');
+			for( name in this.sections ) {
+				this.$el.append( this.sections[name].render() );
+			}
+			return this.$el;
+		}
+	});
+	
+	var nodes = new Nodes();
+		nodes.fetch({
 			reset: true,
 			success: function() {
 				
@@ -46,15 +88,13 @@
 				throw new Error( response );
 			}
 		});
-	
-	
-	categories.on('reset', function() {
+		
+	nodes.on('reset', function() {
+		console.log('reset', nodes.toJSON());
 		
 	});
 	
-	var view = new View({
-		collection: categories,
-	});
+	var view = new HomeScreen({collection: nodes});
 	
 	
 	$(function() {
