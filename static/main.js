4bc8fe90a723110b27ee47c5391986f8b52a2812
ghodir <ghodir@aionlabs.de>
2013-07-27 10:19:01 +0200

    Implemented a Backbone View for the categories

--- static/main.js
+++ static/main.js
@@ -1,3 +1,67 @@
+(function() {
+	var Category = Backbone.Model.extend({
+		defaults: {
+			name: '',
+			type: '',
+			icon: '',
+			value: '',
+			budget: '',
+			color: '',
+		}
+	});
+	
+	var CategoriesCollection = Backbone.Collection.extend({
+		url: '/api/users/1/categories',
+		model: Category,
+	});
+	
+	var Section = Backbone.Model.extend();
+	var SectionsCollection = Backbone.Collection.extend();
+	
+	var View = Backbone.View.extend({
+		template: _.template( $('#template-view').html() ),
+		initialize: function( options ) {
+			options = options || {};
+			
+			this.collection = options.collection;
+			this.collection.on('reset', this.render, this);
+		},
+		render: function( ) {
+			var sections = this.collection.groupBy('type');
+			_.each( sections, function( categories, name ) {
+				sections[name] = ( new CategoriesCollection(categories)).toJSON()
+			});
+			this.$el.html( this.template({sections: sections}) );
+			return this.$el;
+		}
+	});
+	
+	var categories = new CategoriesCollection();
+		categories.fetch({
+			reset: true,
+			success: function() {
+				
+			},
+			error: function(collection, response, options) {
+				throw new Error( response );
+			}
+		});
+	
+	
+	categories.on('reset', function() {
+		
+	});
+	
+	var view = new View({
+		collection: categories,
+	});
+	
+	
+	$(function() {
+		$('#content').html( view.render() );
+	});
+})();
+
 $(function() {
 	$('.category.source').draggable({
 		stack: '.category',
