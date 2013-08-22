b30ebe34f639a0fc172694ca00629ca6c7506c91
ghodir <ghodir@aionlabs.de>
2013-08-22 14:20:22 +0200

    Finished overview

--- static/app.overview.js
+++ static/app.overview.js
@@ -3,46 +3,14 @@ App.populator('overview', function(page, args) {
 	
 	$( page ).on('appShow', function() {
 		$(page).find('.categories').empty();
-		/*
-		var now = new Date(), y = now.getFullYear(), m = now.getMonth();
-		var start = new Date(y, m, 1);
-		var end   = new Date(y, m + 1, 0);
 		
-		
-		var sum = 0.00;
-		var amounts = {};
-		var transactions = CashTrack.request('transactions', { date: {	$gte: start, $lt: end } });
-		_.each( transactions, function( transaction ) {
-			sum += transaction.amount;
-			if( amounts[ transaction.destination ] )
-				amounts[ transaction.destination ] += transaction.amount;
-			else
-				amounts[ transaction.destination ] = transaction.amount;
-		});
-		
-		var max = 0.0;
-		var percentages = [];
-		var categories = CashTrack.request('categories');
-		_.each( categories, function( category ) {
-			category.amount = amounts[ category.id ] / sum;
-			if( category.amount > max )
-				max = category.amount;
-		});
-		
-		categories = _.sortBy( categories, 'amount').reverse();
-		$( page ).find('.month').text( Globalize.format( start, 'MMMM') );
-		$( page ).find('.expenses').text( Globalize.format( sum, 'c' ) );
-		*/
 		var container = $(page).find('.categories').empty();
 		$.when( CashTrack.request('categories', null, 0) )
 		 .then( function( categories ) {
-			var max = 0;
-			categories.sortBy('amount').reverse().each(function( category ) {
-				if( category.amount > max )
-					max = category.amount;
-			});
-			
-			categories.each(function( category ) {
+			$( page ).find('.month').text( Globalize.format( new Date(), 'MMMM') );
+			$( page ).find('.expenses').text( Globalize.format( categories.sum, 'c' ) );
+		
+			categories.sortBy('amount').reverse().each(function( category, index, list) {
 				
 				var item = $(template[0].cloneNode(true));
 					item.data('category', category.id );
@@ -56,7 +24,7 @@ App.populator('overview', function(page, args) {
 					item.find('.bar').css({
 						'background-color': category.color,
 						'border-color': darken( category.color, 50),
-						'width': category.amount / max * 100.0 + '%'
+						'width': category.amount / list[0].amount * 100.0 + '%'
 					});
 			});
 		 });
